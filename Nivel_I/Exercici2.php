635af4491511f1d52f02f86b5eb437803314e189
Oscar Mesejo <oscar.mesejo@gmail.com>
2025-10-20 17:08:36 +0200

    commit ex2

--- Nivel_I/Exercici2.php
+++ Nivel_I/Exercici2.php
@@ -1,5 +1,42 @@
 <?php
 
+echo "El número de línea es: " . __LINE__ . "\n";
+echo "El número de línea es: " . __LINE__ . "\n";
+echo "El número de línea es: " . __LINE__ . "\n";
+
+echo "\n";
+
+echo "El nombre del fichero es: " . __FILE__ . "\n";
+
+echo "\n";
+
+echo "El nombre del directorio es: " . __DIR__ . "\n";
+
+echo "\n";
+
+function constantesMagicas(){
+    echo "El nombre de la función es: " . __FUNCTION__ . "\n";
+}
+
+constantesMagicas();
+
+echo "\n";
+
+class constMagicas{
+    public function claseMagica(){
+        echo "El nombre de la clase es: " . __CLASS__ . "\n";
+    }
+}
+
+echo "\n";
+
+class metodoMagico{
+    public function metodo(){
+        echo "El nombre del método es: " . __METHOD__ . "\n";
+    }
+}
+
+
 
 
 ?>
